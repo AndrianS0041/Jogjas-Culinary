30201dc58c13a57c1c4e9de9e8ae0b52199976bb
hestikapw <hestika.0034@students.amikom.ac.id>
2019-01-03 22:33:10 +0700

    Jogjas-Culinary

---
+++ app/src/main/java/com/jogjasculinary/jogjasculinary/Menu_Makanan_Ringan.java
@@ -0,0 +1,13 @@
+package com.jogjasculinary.jogjasculinary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu_Makanan_Ringan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__makanan__ringan);
+    }
+}
