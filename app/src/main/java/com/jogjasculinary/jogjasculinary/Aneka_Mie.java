58e5a2b6928e4262ca2b7155dcb16cbf879e9ed6
hestikapw <hestika.0034@students.amikom.ac.id>
2019-01-03 23:16:21 +0700

    Jogjas-Culinary

---
+++ app/src/main/java/com/jogjasculinary/jogjasculinary/Aneka_Mie.java
@@ -0,0 +1,13 @@
+package com.jogjasculinary.jogjasculinary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aneka_Mie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aneka__mie);
+    }
+}
