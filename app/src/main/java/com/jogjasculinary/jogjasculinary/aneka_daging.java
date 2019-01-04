78eb5bb0c166dbd35c88be48fcb469b7f2587a2a
karinanovita <karinanovita222@gmail.com>
2019-01-04 14:52:11 +0700

    Jogjas-Culinary

---
+++ app/src/main/java/com/jogjasculinary/jogjasculinary/aneka_daging.java
@@ -0,0 +1,13 @@
+package com.jogjasculinary.jogjasculinary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aneka_daging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aneka_daging);
+    }
+}
---
+++ app/src/main/java/com/jogjasculinary/jogjasculinary/aneka_nasi.java
@@ -0,0 +1,13 @@
+package com.jogjasculinary.jogjasculinary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aneka_nasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aneka_nasi);
+    }
+}
